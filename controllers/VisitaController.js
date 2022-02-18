e32a2231ff90857420ab8b1010bc62e889c5e72d
akyrasouza <akyrapop@hotmail.com>
2022-02-18 17:26:51 -0300

    listar visitas

--- controllers/VisitaController.js
+++ controllers/VisitaController.js
@@ -1,12 +1,19 @@
 const Visita = require("../models/Visita");
 
 class VisitaController {
+
     static async addVisita(req, res) {
         const { data, visitante} = req.body
         const visita = Visita({ data, visitante });
         await visita.save(); //solicitação remota ao banco
 
         res.status(201).json({ message: 'Visita adicionada com sucesso' })
+    }
+
+    static async listarVisita(req, res) {
+
+        const visitas = await Visita.find().lean();
+        res.json(visitas);
     }
 
 }
--- routes/visitaRoutes.js
+++ routes/visitaRoutes.js
@@ -3,6 +3,7 @@ const VisitaController = require("../controllers/VisitaController")
 const router = Router();
 
 router.post("/add/visita", VisitaController.addVisita);
+router.get("/visita",VisitaController.listarVisita);
 
 
 module.exports = router

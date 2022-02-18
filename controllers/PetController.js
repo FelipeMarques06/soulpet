1e00c5cddff835f3fa35c43f658e1cb049607230
brunodiascultural <brunodiascultural@gmail.com>
2022-02-18 15:27:30 -0300

    Edit Pet

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -1,19 +1,26 @@
 const Pet = require('../models/Pet')
 
-class PetController{
+class PetController {
 
-    static async addPet(req, res){
-        const {nome, raca,cor,idade,status} = req.body
-        const pet = Pet({nome, raca,cor,idade,status});
+    static async addPet(req, res) {
+        const { nome, raca, cor, idade, status } = req.body
+        const pet = Pet({ nome, raca, cor, idade, status });
         await pet.save(); //solicitação remota ao banco
 
-       res.status(201).json({message: 'Pet criado com sucesso'})
+        res.status(201).json({ message: 'Pet criado com sucesso' })
     }
 
-    static async listarPet(req, res){
-       
+    static async listarPet(req, res) {
+
         const pets = await Pet.find().lean();
         res.json(pets);
+    }
+    // Edit Pet
+    static async editPet(req, res) {
+        const { id, nome, raca, cor, idade, status } = req.body
+        await Pet.findByIdAndUpdate(id, { nome, raca, cor, idade, status })
+
+        res.status(201).json({ message: 'Pet atualizado com sucesso!' })
     }
 }
 
--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -2,8 +2,10 @@ const { Router } = require("express");
 const PetController = require("../controllers/PetController")
 const router = Router();
 
-router.post('/add/pet',PetController.addPet);
+router.post('/add/pet', PetController.addPet);
 
 router.get('/pet', PetController.listarPet);
+
+router.post('/edit/pet/:id', PetController.editPet);
 
 module.exports = router

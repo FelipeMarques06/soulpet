8bacd170099cd51af0064ab0f8aeac9dee686cd0
akyrasouza <akyrapop@hotmail.com>
2022-02-19 17:30:52 -0300

    relacionamento entre entidades

--- controllers/VisitaController.js
+++ controllers/VisitaController.js
@@ -1,10 +1,11 @@
 const Visita = require("../models/Visita");
+const Pet = require('../models/Pet')
 
 class VisitaController {
 
     static async addVisita(req, res) {
-        const { data, visitante} = req.body
-        const visita = Visita({ data, visitante });
+        const { data, visitante,pet} = req.body
+        const visita = Visita({ data, visitante,pet});
         await visita.save(); //solicitação remota ao banco
 
         res.status(201).json({ message: 'Visita adicionada com sucesso' })
@@ -12,7 +13,7 @@ class VisitaController {
 
     static async listarVisita(req, res) {
 
-        const visitas = await Visita.find().lean();
+        const visitas = await Visita.find().populate('pet').lean();
         res.json(visitas);
     }
 
